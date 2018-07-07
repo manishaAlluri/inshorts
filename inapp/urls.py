89df270e55defbdefe704b8c1a61b5fc7d45a52e
manishaAlluri <39620995+manishaAlluri@users.noreply.github.com>
2018-07-07 20:09:50 +0530

    Update urls.py

--- inapp/urls.py
+++ inapp/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.latest_news, name = 'latest_news'),
-    path('sports/', views.sports_news, name = 'sports_news'),
-    path('business/', views.business_news, name = 'business_news'),
-    path('entertainment/', views.entertainment_news, name = 'entertainment_news'),
-    path('health/', views.health_news, name = 'health_news'),
-    path('technology/', views.technology_news, name = 'technology_news'),
-    path('general/', views.general_news, name = 'general_news'),
-    path('science/', views.science_news, name = 'science_news'),
-    path('search/', views.search_news, name = 'search_news'),
+    url('', views.latest_news, name = 'latest_news'),
+    url('sports/', views.sports_news, name = 'sports_news'),
+    url('business/', views.business_news, name = 'business_news'),
+    url('entertainment/', views.entertainment_news, name = 'entertainment_news'),
+    url('health/', views.health_news, name = 'health_news'),
+    url('technology/', views.technology_news, name = 'technology_news'),
+    url('general/', views.general_news, name = 'general_news'),
+    url('science/', views.science_news, name = 'science_news'),
+    url('search/', views.search_news, name = 'search_news'),
 ]
